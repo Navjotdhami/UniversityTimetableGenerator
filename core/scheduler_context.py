c5c38079c728c05ce08365c23a3f3c1f7eb2c4fa
Navjot Dhami <dhami.navjot@gmail.com>
2026-07-07 19:06:43 +0530

    Extend SchedulerContext for constraint scheduling

--- core/scheduler_context.py
+++ core/scheduler_context.py
@@ -3,17 +3,22 @@ from dataclasses import dataclass, field
 
 @dataclass
 class SchedulerContext:
-
+    # Master Data
     courses: list = field(default_factory=list)
-
     sections: list = field(default_factory=list)
-
     faculties: list = field(default_factory=list)
-
     rooms: list = field(default_factory=list)
-
     labs: list = field(default_factory=list)
 
+    # Generated Data
     timeslots: list = field(default_factory=list)
+    activities: list = field(default_factory=list)
 
+    # Scheduler Output
+    assignments: list = field(default_factory=list)
     scheduled_classes: list = field(default_factory=list)
+
+    # Runtime Calendars
+    faculty_schedule: dict = field(default_factory=dict)
+    room_schedule: dict = field(default_factory=dict)
+    section_schedule: dict = field(default_factory=dict)
